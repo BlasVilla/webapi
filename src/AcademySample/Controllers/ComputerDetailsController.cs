4c2466dcca535ade7717b8877f3c304adbdecc13
Blas Villa <b.villa@loginvsi.com>
2016-08-09 13:49:31 +0200

    Initial push

---
+++ src/AcademySample/Controllers/ComputerDetailsController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using AcademySample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademySample.Controllers
+{
+    [Route("api/computers")]
+    public class ComputerDetailsController : Controller
+    {
+
+        [HttpGet]
+        [Route("", Name = "GetAllComputerDetails")]
+        public ComputerDetails[] GetAll()
+        {
+            return DummyData.Computers.ToArray();
+        }
+
+        [HttpGet]
+        [Route("{computerId}", Name = "GetComputerById")]
+        public ComputerDetails GetById(string computerId)
+        {
+            return DummyData.Computers.SingleOrDefault(c => c.Name == computerId);
+        }
+
+        [HttpDelete]
+        [Route("{computerId}")]
+        public IActionResult Delete(string computerId)
+        {
+            var computer = DummyData.Computers.SingleOrDefault(c => c.Name == computerId);
+
+            if (computer != null)
+            {
+                DummyData.Computers.Remove(computer);
+            }
+
+            return Ok();
+        }
+    }
+
+    public class DummyData
+    {
+        public static IList<ComputerDetails> Computers { get; }
+
+        static DummyData()
+        {
+            Computers = new List<ComputerDetails>(new [] { new ComputerDetails { Name = "computer 1" },
+                new ComputerDetails { Name = "computer 2" }});
+        }
+    }
+}
---
+++ src/AcademySample/Models/ComputerDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace AcademySample.Models
+{
+    public class ComputerDetails
+    {
+        public string Name { get; set; }
+
+        public string IpAddress { get; set; }
+
+        public int Memory { get; set; }
+
+        public string User { get; set; }
+    }
+}
